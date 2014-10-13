7ce2c3d8b679ac55ea5ca4c9de0cbb202b13f0ab
Ilya <lyntik22@gmail.com>
2014-10-13 20:16:49 +0300

    work

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,7 +5,7 @@ console.log('APPLICATION START');
 $('.qrCodeEl').on('click', function(e) {
 	var eventId = e.target.getAttribute('data-eventId'),
 		popup = window.open(),
-		imageUrl = ('src', 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=http://epbyminw3632:1337/events/show/' + eventId);
+		imageUrl = ('src', 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=http://'+ window.location.host+ '/events/show/' + eventId);
 	popup.document.write('<div style="margin: 0 auto; text-align:center">');
 	popup.document.write('<h1>Check In RIGHT NOW!<h1>');
 	popup.document.write('<img src='+imageUrl+'>');
